cdebdbbc33ad9a4956f6eef1696cf6906910c543
mohanjha2907 <mohanjha0705@gmail.com>
2026-04-11 00:49:51 +0530

    added vercel url

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,8 +19,6 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://faceflow-two.vercel.app",
-    "https://faceflow-git-main-mohan-kumar-jhas-projects.vercel.app",
-    "https://faceflow-qqg6055iy-mohan-kumar-jhas-projects.vercel.app"
   ],
   credentials: true
 }))
